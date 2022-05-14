a3e41dc974bb3c5c6fb22cb9186104f976a99522
Zheng Can <zc3593835@gmail.com>
2022-05-14 13:29:30 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,10 +7,14 @@ if [[ ! -x $(command -v $DOSBOX) ]]; then
 	if [[ ! -e $DOSBOX ]]; then
 		DOSBOX_X_VERSION=0.83.25
 		DOSBOX_X_BUILDNO=20220501074941
-		wget -P temp https://github.com/joncampbell123/dosbox-x/releases/download/dosbox-x-v${DOSBOX_X_VERSION}/dosbox-x-macosx-$(arch)-${DOSBOX_X_BUILDNO}.zip
-		unzip temp/*.zip -d temp
-		mv temp/dosbox-x/dosbox-x.app .
-		rm -rf temp
+		ARCH=$(arch)
+		if [[ $ARCH === "i386" ]]; then
+			ARCH="x86_64"
+		fi
+		wget -P tt_temp https://github.com/joncampbell123/dosbox-x/releases/download/dosbox-x-v${DOSBOX_X_VERSION}/dosbox-x-macosx-$(arch)-${DOSBOX_X_BUILDNO}.zip
+		unzip tt_temp/*.zip -d tt_temp
+		mv tt_temp/dosbox-x/dosbox-x.app .
+		rm -rf tt_temp
 	fi
 fi
 
@@ -20,6 +24,11 @@ if [[ ! -e $TT ]]; then
 	wget -P tt https://raw.githubusercontent.com/zhengcan/tt-on-mac/master/tt.zip
 	unzip tt/*.zip -d tt
 	rm tt/*.zip
+fi
+
+# Download self
+if [[ ! -e `pwd`/run.sh ]]; then
+	wget https://raw.githubusercontent.com/zhengcan/tt-on-mac/master/run.sh
 fi
 
 # Run
